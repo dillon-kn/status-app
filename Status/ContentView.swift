790199e180ae25f9608fd90a4abb93c3803d921a
Dillon Nguyen <dillonkn@stanford.edu>
2024-06-19 09:39:13 -0700

    Initial Commit

---
+++ Status/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Status
+//
+//  Created by Dillon Nguyen on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Status/StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusApp.swift
+//  Status
+//
+//  Created by Dillon Nguyen on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

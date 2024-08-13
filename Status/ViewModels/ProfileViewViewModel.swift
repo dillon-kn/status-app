bc9d3ff33cd3502f071fa89395df9f868ff64096
Dillon Nguyen <dillonkn@stanford.edu>
2024-08-13 11:30:06 -0700

    Added Log Out Functionality

--- Status/ViewModels/ProfileViewViewModel.swift
+++ Status/ViewModels/ProfileViewViewModel.swift
@@ -6,7 +6,51 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewViewModel: ObservableObject {
+    @Published var showAlert = false
+    @Published var errorTitle = ""
+    @Published var errorMessage = ""
+    @Published var user: User? = nil
     
+    init() {
+        
+    }
+    
+    func fetchUser() {
+        guard let userID = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users").document(userID).getDocument { [weak self] snapshot, error in
+            guard let data = snapshot?.data(), error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self?.user = User(
+                    id: data["id"] as? String ?? "",
+                    firstName: data["firstName"] as? String ?? "",
+                    lastName: data["lastName"] as? String ?? "",
+                    username: data["username"] as? String ?? "",
+                    email: data["email"] as? String ?? "",
+                    phoneNumber: data["phoneNumber"] as? String ?? "",
+                    joined: data["joined"] as? Timestamp ?? Timestamp()
+                )
+            }
+            
+        }
+    }
+    
+    func logOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            showAlert = true
+            errorTitle = "Error Logging Out"
+            errorMessage = "Please contact support"
+        }
+    }
 }
--- Status/Views/ProfileView.swift
+++ Status/Views/ProfileView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ProfileView: View {
     @StateObject var viewModel = ProfileViewViewModel()
+    @StateObject var colorModel = ColorViewModel()
+    
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
                 
+                SButton(
+                    title: "Log Out",
+                    background: Color(hex: colorModel.forestGreen),
+                    action: {
+                        viewModel.logOut()
+                })
+                .frame(width: 100, height: 50)
+                .padding()
             }
+            .alert(viewModel.errorTitle, isPresented: $viewModel.showAlert, presenting: viewModel.errorMessage) { errorMessage in
+                Button("OK", role: .cancel) {
+                    // Message dismisses
+                }
+            } message: { errorMessage in
+                Text(errorMessage)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(Color(hex: colorModel.lightCream))
+            .edgesIgnoringSafeArea(.all)
         }
     }
 }
